7c9492e23ff4d2fdeb296d1aeeabab6dc0a956ba
M Ahmed Mushtaq <mmushtaq.bese19seecs@seecs.edu.pk>
2020-07-18 22:32:02 +0500

    Authorization Complete

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="ui secondary pointing menu">
+
+        <a href="/" class="active item">
+            Image Storage
+        </a>
+        <div class="right menu">
+            <div v-if="isLoggedIn">
+                <button>Gallery Button</button>
+                <button>Image Upload</button>
+               <button @click="logout">Logout</button>
+            </div>
+            <a v-else href="#" class="ui item" @click="login">
+                Login
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {mapActions,mapGetters} from "vuex";
+
+    export default {
+        name:'AppHeader',
+        computed:{
+            ...mapGetters(['isLoggedIn'])
+        },
+        methods:{
+            ...mapActions(['login','logout']),
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App";
+import store from "./store";
+import VueRouter from "vue-router";
+import AuthHandler from "./components/AuthHandler";
 
-Vue.config.productionTip = false
+
+Vue.use(VueRouter);
+export const router = new VueRouter({
+    mode:'history',
+    routes:[
+        {path:'/oauth2/callback',component:AuthHandler}
+    ]
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+   router,
+   store,
+    render:h=>h(App),
+
+}).$mount('#app');
+
